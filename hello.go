d39bbb6cb35d4589bb5ba6e671ea17cc2953658e
Christian Schaefer <chrisse.s@googlemail.com>
2019-05-13 21:28:55 +0200

    Working scanner

--- hello.go
+++ hello.go
@@ -3,74 +3,24 @@ package main
 import (
 	"crypto/tls"
 	"fmt"
-	"hostscanner"
-	"net"
-	"sync"
-	"time"
+
+	"github.com/schaefec/portdiscovery/hostscanner"
 )
 
 func main() {
 
-	hostPort := make([]string, 0, 0)
-
-	hostPort = append(hostPort, "localhost:4433")
-	hostPort = append(hostPort, "127.0.0.1:4443")
-
-	new(hostscanner.Scanner)
-
-	channel := make(chan tls.ConnectionState, 1024)
-	done := make(chan bool)
-
-	var wg sync.WaitGroup
-	for _, hp := range hostPort {
-		wg.Add(1)
-		go dialTLS(hp, channel, &wg)
-
-	}
-	go printState(channel, done)
-
-	wg.Wait() // Block until all producers have been terminated.
-
-	close(channel)
-
-	<-done
-
-	fmt.Println("Exiting...")
-}
-
-func printState(statechan <-chan tls.ConnectionState, done chan<- bool) {
-	for {
-		state, more := <-statechan
-		if more {
-			for _, cert := range state.PeerCertificates {
-				fmt.Println(cert.Subject)
-			}
-		} else {
-			done <- true
+	t := hostscanner.NewScanner(func(state tls.ConnectionState) {
+		for _, cert := range state.PeerCertificates {
+			fmt.Println(cert.Subject)
 		}
-	}
-}
-
-func dialTLS(hostport string, ch chan<- tls.ConnectionState, wg *sync.WaitGroup) {
-	defer wg.Done()
-	fmt.Println("Connecting to:", hostport)
-	conn, err := tls.DialWithDialer(&net.Dialer{
-		Timeout: 500 * time.Millisecond,
-	}, "tcp", hostport, &tls.Config{
-		InsecureSkipVerify: true,
 	})
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
 
-	defer conn.Close()
+	t.Enqueue("127.0.0.1:4433")
+	t.Enqueue("127.0.0.2:4433")
+	t.Enqueue("127.0.0.3:4433")
+	t.Enqueue("127.0.0.4:4433")
+	t.Enqueue("127.0.0.5:4433")
+	t.Enqueue("localhost:4433")
 
-	conn.Handshake()
-
-	fmt.Println("Connection to:", hostport, "established")
-
-	state := conn.ConnectionState()
-
-	ch <- state
+	t.CloseAndAwaitTermination()
 }
--- hostscanner/hostscanner.go
+++ hostscanner/hostscanner.go
@@ -9,22 +9,67 @@ import (
 )
 
 // Scanner represents a port-scanner on tcp ports.
-type Scanner struct {
-	WaitGroup *sync.WaitGroup
-	States    chan<- tls.ConnectionState
+type Scanner interface {
+	// Enqueue adds an additional hostport to to the list of to be scanned hosts
+	Enqueue(hostport string) error
+	// Closes this scanner and waits until all results has been processed
+	CloseAndAwaitTermination()
 }
 
-// New creates a new scanner
-func New(wg *sync.WaitGroup) *Scanner {
-	return &Scanner{
-		WaitGroup: wg,
-		States:    make(chan tls.ConnectionState, 1024),
+type scanner struct {
+	waitGroup      *sync.WaitGroup
+	states         chan tls.ConnectionState
+	done           chan bool
+	closeTriggered bool
+}
+
+// NewScanner creates a new scanner
+func NewScanner(accept func(tls.ConnectionState)) Scanner {
+	scanner := &scanner{
+		waitGroup:      &sync.WaitGroup{},
+		states:         make(chan tls.ConnectionState, 1024),
+		done:           make(chan bool),
+		closeTriggered: false,
 	}
+
+	go consume(scanner.states, scanner.done, accept)
+
+	return scanner
 }
 
-// Enqueue adds an additional hostport to to the list of to be scanned hosts
-func (s *Scanner) Enqueue(hostport string) {
-	go dialTLSInternal(hostport, s.States, s.WaitGroup)
+func (s *scanner) Enqueue(hostport string) error {
+	if s.closeTriggered {
+		return fmt.Errorf("Enqueue cannot be called when scanner has been closed already")
+	}
+	s.waitGroup.Add(1)
+	go dialTLSInternal(hostport, s.states, s.waitGroup)
+
+	return nil
+}
+
+func (s *scanner) CloseAndAwaitTermination() {
+	if s.closeTriggered {
+		return
+	}
+
+	s.closeTriggered = true
+
+	fmt.Print("Exiting...")
+	s.waitGroup.Wait()
+	close(s.states)
+	<-s.done
+	fmt.Println("done!")
+}
+
+func consume(ch <-chan tls.ConnectionState, done chan<- bool, accept func(tls.ConnectionState)) {
+	for {
+		state, more := <-ch
+		if more {
+			accept(state)
+		} else {
+			done <- true
+		}
+	}
 }
 
 func dialTLSInternal(hostport string, ch chan<- tls.ConnectionState, wg *sync.WaitGroup) {
